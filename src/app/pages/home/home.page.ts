85ee1da9939ac256e67dd1d4c909b6782b1324dd
ferny06 <fer.irarrazabal@duocuc.cl>
2026-03-30 22:15:17 -0300

    avance page home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../../services/movie';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  standalone: false,
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  standalone: false // Esto es importante según tu versión de Ionic
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  peliculas: any[] = [];
 
+  constructor(private movieService: MovieService) { }
+
+  ngOnInit() {
+    this.movieService.getPopularMovies().subscribe({
+      next: (data: any) => {
+        this.peliculas = data.results;
+        console.log('Películas cargadas:', this.peliculas);
+      },
+      error: (err) => {
+        console.error('Error al cargar pelis:', err);
+      }
+    });
+  }
 }
